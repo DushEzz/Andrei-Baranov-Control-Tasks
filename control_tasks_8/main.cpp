920a1b15257004e43055eb85fd0708ba6f2063bd
Andrew <dushez.baranov@gmail.com>
2022-11-27 12:40:48 +0300

    Added control_tasks_8

---
+++ control_tasks_8/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include "time.h"
+
+using namespace std;
+
+int main()
+{
+    int hours, minutes, seconds;
+
+    cout << "Hours: "; cin >> hours;
+    cout << "Minutes: "; cin >> minutes;
+    cout << "Seconds: "; cin >> seconds;
+
+    Time time_1 = Time(hours, minutes, seconds);
+    Time const time_2 = Time(45, 1, 87);
+    Time const time_3 = Time(5, 0, 12);
+    Time time_4 = Time().add(time_2, time_3);
+
+    cout << "Inputed time: ";
+    time_1.show_time();
+    cout << "Result time: ";
+    time_4.show_time();
+
+    return 0;
+}
